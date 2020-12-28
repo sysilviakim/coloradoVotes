a0ce0b1f715f9a4b8264de31305d27dba4b403f5
Seo-young Silvia Kim <sskim.research@gmail.com>
2020-12-27 23:36:15 -0500

    instead of specifying file names, search .txt

--- R/01_data_import.R
+++ R/01_data_import.R
@@ -17,22 +17,15 @@ list.files(
 
 # Data import ==================================================================
 elect <- list(
-  returned = file.path(
-    "data", "raw", "CE-068c_Voters_With_Returned_Ballot_List_Public",
-    "CE-068c_Voters_With_Returned_Ballot_List_Public_03Nov_600015773_null.txt"
-  ),
-  cured = file.path(
-    "data", "raw", "CE-077_Rejected_Cure",
-    "CE-077_Rejected_Cure_03Nov_600015773_20201111_115002.txt"
-  ),
-  undelivered = file.path(
-    "data", "raw", "CE-037_UndeliverableBallots",
-    "CE-037_UndeliverableBallots_03Nov_600015773_20201111_232502.txt"
-  )
+  returned = 
+    file.path("data", "raw", "CE-068c_Voters_With_Returned_Ballot_List_Public"),
+  cured = file.path("data", "raw", "CE-077_Rejected_Cure"),
+  undelivered = file.path("data", "raw", "CE-037_UndeliverableBallots")
 ) %>% 
   map(
     ~ read.table(
-      .x, header = TRUE, sep = "|", na.strings = "", colClasses = "character",
+      list.files(.x, pattern = ".txt"), 
+      header = TRUE, sep = "|", na.strings = "", colClasses = "character",
       fill = TRUE, stringsAsFactors = TRUE, comment.char = "",
       nrows = nrows
     ) %>%
