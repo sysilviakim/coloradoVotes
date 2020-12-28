7eadd19ff6d624bd5e5006b6be8be0299d67ccf6
Seo-young Silvia Kim <sskim.research@gmail.com>
2020-12-28 02:13:27 -0500

    add Kmisc import

--- R/00_utilities.R
+++ R/00_utilities.R
@@ -2,9 +2,15 @@
 library(tidyverse)
 library(lubridate)
 library(janitor)
+# remotes::install_github(
+#   "sysilviakim/Kmisc", INSTALL_opts = c("--no-multiarch")
+# )
+library(Kmisc) 
 
 if (Sys.info()["sysname"] == "Windows") {
   nrows <- -1
 } else {
   nrows <- 100
 }
+
+# Functions ====================================================================
