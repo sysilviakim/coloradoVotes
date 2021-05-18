55c7883cb86afa2196f48a43ffb627825fb7b706
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-05-18 12:15:20 -0400

    cosmetics + add sanity check

--- R/covid_co_counties.R
+++ R/covid_co_counties.R
@@ -5,22 +5,26 @@ source(here::here("R", "utilities.R"))
 # Updated periodically
 # https://github.com/nytimes/covid-19-data/tree/master/rolling-averages
 
-link <- paste0("https://raw.githubusercontent.com/nytimes/",
-               "covid-19-data/master/rolling-averages/us-counties-recent.csv")
+link <- paste0(
+  "https://raw.githubusercontent.com/nytimes/",
+  "covid-19-data/master/rolling-averages/us-counties-recent.csv"
+)
 
 covid_data <- read_csv(link)
 
 # Population Data ==============================================================
 # Data from census county population estimates
 
-link2 <- paste0("https://www2.census.gov/programs-surveys/popest/datasets/",
-                "2010-2020/counties/totals/co-est2020-alldata.csv")
+link2 <- paste0(
+  "https://www2.census.gov/programs-surveys/popest/datasets/",
+  "2010-2020/counties/totals/co-est2020-alldata.csv"
+)
 
 population <- read_csv(link2) %>%
   clean_names()
 
 # Cleaning =====================================================================
-# Filtering for Colorado counties only 
+# Filtering for Colorado counties only
 covid_colorado <- covid_data %>%
   filter(state %in% "Colorado")
 
@@ -32,9 +36,9 @@ co_population <- population %>%
 
 # Checking counties
 assert_that(length(setdiff(unique(covid_colorado$county), NA)) == 64)
-assert_that(length(setdiff(unique(co_population$ctyname), "Colorado")) == 64)
-# All counties are accounted for. In the population data, there is one 
-# row for the total population of Colorado in the county column. 
+assert_that(length(setdiff(unique(co_population$county), "Colorado")) == 64)
+# All counties are accounted for. In the population data, there is one
+# row for the total population of Colorado in the county column.
 
 # Joining the data
 co_covid <- inner_join(co_population, covid_colorado, by = "county")
@@ -45,15 +49,15 @@ co_covid <- co_covid %>%
 
 # Calculating proportions ======================================================
 co_covid <- co_covid %>%
-  mutate(cases_per_million = cases/(population/1e6),
-         deaths_per_million = deaths/(population/1e6),
-         cases_per_100k = cases/(population/1e5),
-         deaths_per_100k = deaths/(population/1e5),
-         cases_per_10k = cases/(population/1e4),
-         deaths_per_10k = deaths/(population/1e4))
+  mutate(
+    cases_per_million = cases / (population / 1e6),
+    deaths_per_million = deaths / (population / 1e6),
+    cases_per_100k = cases / (population / 1e5),
+    deaths_per_100k = deaths / (population / 1e5),
+    cases_per_10k = cases / (population / 1e4),
+    deaths_per_10k = deaths / (population / 1e4)
+  )
 
 # Saving =======================================================================
+assert_that(n_distinct(co_covid$county) == 64)
 save(co_covid, file = here("data", "tidy", "co_county_covid.Rda"))
-
-
-
