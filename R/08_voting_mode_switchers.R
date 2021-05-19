88f3b06580f26d3d1e68fbe11b83deb73d2edff2
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-05-19 16:02:34 -0400

    min. switcher + condprob fxn

--- R/08_voting_mode_switchers.R
+++ R/08_voting_mode_switchers.R
@@ -7,3 +7,37 @@ if (nrows == 100) {
   load(here("data", "tidy", "voter_history_wide_full.RData"))
 }
 
+# Create switchers (init. code) ================================================
+df <- voter_history_wide %>%
+  select(
+    voter_id, gen2020, pri2020, gen2018, pri2018,
+    gen2016, pri2016, gen2014, pri2014,
+    zip = residential_zip, party, county = county_name
+  ) %>%
+  arrange(desc(gen2020), voter_id) %>%
+  mutate(
+    switcher = case_when(
+      gen2020 == 1 & (
+        (is.na(pri2020) | !is.na(pri2020) & pri2020 == 0) &
+          (is.na(gen2018) | !is.na(gen2018) & gen2018 == 0) &
+          (is.na(pri2018) | !is.na(pri2018) & pri2018 == 0) &
+          (is.na(gen2016) | !is.na(gen2016) & gen2016 == 0) &
+          (is.na(pri2016) | !is.na(pri2016) & pri2016 == 0) &
+          (is.na(gen2014) | !is.na(gen2014) & gen2014 == 0) &
+          (is.na(pri2014) | !is.na(pri2014) & pri2014 == 0)
+      ) &
+        !(
+          is.na(gen2018) & is.na(pri2018) & is.na(gen2016) &
+            is.na(pri2016) & is.na(gen2014) & is.na(pri2014)
+        )
+      ~ 1,
+      is.na(gen2020) ~ NA_real_,
+      TRUE ~ 0
+    )
+  )
+
+# By-party conditional probability =============================================
+pretty_condprob(df, "switcher", 1, "party", "dem", digits = 2)
+pretty_condprob(df, "switcher", 1, "party", "rep", digits = 2)
+pretty_condprob(df, "switcher", 1, "party", "uaf", digits = 2)
+
--- R/utilities.R
+++ R/utilities.R
@@ -114,6 +114,30 @@ check_loop <- function(i) {
   }
 }
 
+# Conditional probability (adopted from hiddenDonorsELJ)
+pretty_condprob <- function(df, A_var, A_val, B_var, B_val,
+                            output = "string", digits = 1, percent = TRUE) {
+  B <- df %>%
+    filter(!!as.name(B_var) == B_val)
+  A <- df %>%
+    filter((!!as.name(A_var) == A_val) & (!!as.name(B_var) == B_val))
+  if (output != "string") {
+    return(nrow(A) / nrow(B))
+  } else {
+    message(
+      paste0(
+        "Cond. on ", B_var, " == ", B_val, ", Pr(",
+        A_var, " == ", A_val, ") is ", 
+        ifelse(
+          percent == TRUE, 
+          paste0(round(nrow(A) / nrow(B) * 100, digits = digits), "%"),
+          round(nrow(A) / nrow(B), digits = digits)
+        )
+      )
+    )
+  }
+}
+
 # Define file directory ========================================================
 # Need relative paths so not here::here
 file_paths <- list(
