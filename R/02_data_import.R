255079635c896211d6946ca1576af448974f2ffe
Akhil Bandreddi <ab6668a@student.american.edu>
2021-06-15 18:20:01 -0500

    Update Master Vr to Gen 2020 File.

--- R/02_data_import.R
+++ R/02_data_import.R
@@ -48,47 +48,71 @@ temp <- temp[-(idx + 1), ]
 elect_list$cured <- temp
 
 # Data import/clean: voter registration data ===================================
-master_vr <- list.files(
-  file.path("data", "raw", "EX-003 Master Voter List"),
-  pattern = "Registered.*txt", full.names = TRUE
-) %>%
-  map(
-    ~ readr::read_csv(
-      .x,
-      na = "", col_types = cols(.default = "c"),
-      n_max = ifelse(nrows == -1, Inf, nrows),
-      locale = readr::locale(encoding = "UTF-8")
-      # If read.delim, quote = ""; otherwise, quote EOF error and misses lines
-      # That is to say, there are unclosed double quotes
-      # e.g., "State House 48","N"08468","Yes","Congressional 1",
-      # This is file 5, voter ID 601488340 (in fact... has more columns here)
-      # Half a record deleted? Residue from manual intervention?
-      # Not using read.delim because of extra comma not in header; see
-      # https://stackoverflow.com/questions/8854046/
-      # Why? Has data with a comma inside, while using commas as delimiters
-      # e.g., "Kim", "Silvia", "Kim, Silvia", ...
-      # Encoding issue as well: CEDEÃ`O TORRES or 18/2 RUTESHEIMER STRAÃYE
-      # Annnnnd there are line breaks in the middle of quotes
-      # e.g., Part 1 line 11824--11825 (voter ID 639278)
-      # "Rio Grande
-      # Swatch" ---> good thing is, readr::read_csv correctly recognizeds this
-      # as a single data row
-      # Poorly formatted file overall (facepalm)
-      # Approx. 500000 * 8 + (178493 - 1) for Nov 5, 2020 snapshot
-    )
-  ) %>%
-  bind_rows(.id = "file") %>%
-  clean_names()
+files <- list.files(path = here("data", "raw", "EX-003 Master Voter List",
+                                "2020_General"), pattern = "*.txt",
+                    full.names = TRUE)
+
+# Using the same proccess used for vf: 
+master_vr <- vector("list", length(files))
+
+# Loop to merge
+for (i in 1:length(files)) {
+  master_vr[[i]] <- files[[i]] %>%
+    set_names(.) %>%
+    map_dfr(
+      ~ read.table(.x, sep = ",", header = TRUE),
+      .id = "gen2020_file"
+    ) %>%
+    clean_names()
+}
+
+# Bind 
+master_vr <- list.rbind(master_vr) %>%
+  mutate(across(.cols = everything(), tolower),
+         across(.cols = everything(), as.character)) 
+
+# master_vr <- list.files(
+#   file.path("data", "raw", "EX-003 Master Voter List"),
+#   pattern = "Registered.*txt", full.names = TRUE
+# ) %>%
+#   map(
+#     ~ readr::read_csv(
+#       .x,
+#       na = "", col_types = cols(.default = "c"),
+#       n_max = ifelse(nrows == -1, Inf, nrows),
+#       locale = readr::locale(encoding = "UTF-8")
+#       # If read.delim, quote = ""; otherwise, quote EOF error and misses lines
+#       # That is to say, there are unclosed double quotes
+#       # e.g., "State House 48","N"08468","Yes","Congressional 1",
+#       # This is file 5, voter ID 601488340 (in fact... has more columns here)
+#       # Half a record deleted? Residue from manual intervention?
+#       # Not using read.delim because of extra comma not in header; see
+#       # https://stackoverflow.com/questions/8854046/
+#       # Why? Has data with a comma inside, while using commas as delimiters
+#       # e.g., "Kim", "Silvia", "Kim, Silvia", ...
+#       # Encoding issue as well: CEDEÃ`O TORRES or 18/2 RUTESHEIMER STRAÃYE
+#       # Annnnnd there are line breaks in the middle of quotes
+#       # e.g., Part 1 line 11824--11825 (voter ID 639278)
+#       # "Rio Grande
+#       # Swatch" ---> good thing is, readr::read_csv correctly recognizeds this
+#       # as a single data row
+#       # Poorly formatted file overall (facepalm)
+#       # Approx. 500000 * 8 + (178493 - 1) for Nov 5, 2020 snapshot
+#     )
+#   ) %>%
+#   bind_rows(.id = "file") %>%
+#   clean_names()
 
 # Was this data imported without error? ========================================
-assert_that(unique(master_vr$residential_state) == "CO")
+assert_that(unique(master_vr$residential_state) == "co")
 assert_that(length(unique(master_vr$county)) == 64)
-assert_that(all(unique(master_vr$status_code) %in% c("A", "I")))
-assert_that(all(unique(master_vr$gender) %in% c("Female", "Male", "Unknown")))
-assert_that(all(unique(master_vr$permanent_mail_in_voter) %in% c("Yes", "No")))
+assert_that(all(unique(master_vr$status_code) %in% c("a", "i")))
+assert_that(all(unique(master_vr$gender) %in% c("female", "male", "unknown")))
+assert_that(all(unique(master_vr$permanent_mail_in_voter) %in% c("yes", "no")))
 assert_that(
-  all(unique(master_vr$congressional) %in% paste0("Congressional ", seq(7)))
+  all(unique(master_vr$congressional) %in% paste0("congressional ", seq(7)))
 )
+# Reran all the assert_thats after loading the new master_vr, they are all true. 
 
 master_vr <- master_vr %>%
   mutate(
@@ -103,7 +127,7 @@ master_vr <- master_vr %>%
   ) %>%
   mutate_if(is.character, ~ tolower(gsub("\"", "", .)))
 
-assert_that(all(unique(master_vr$id_required) %in% c("Y", "N")))
+assert_that(all(unique(master_vr$id_required) %in% c("y", "n")))
 
 # https://www.sos.state.co.us/pubs/elections/VoterRegNumbers/
 # 2020/October/VoterCountsByStatus.pdf
