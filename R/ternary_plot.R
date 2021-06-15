11f013738ce34da3576729b074f8a3774ba22708
Akhil Bandreddi <ab6668a@student.american.edu>
2021-06-15 14:11:48 -0500

    Make changes as per feedback.

--- R/ternary_plot.R
+++ R/ternary_plot.R
@@ -1,48 +1,51 @@
 source(here::here("R", "utilities.R"))
 
 # Import Data -------------------------------------------------------------
-# The cleaned data doesn't have the specific dates the people cast their votes, 
-# going back to the raw joined data for that information:
-load("data/tidy/sample/joined_sample_10k.RData")
 
-# Also loading clean data: 
-vote_wide <- load(here("data", "tidy", "voter_history_wide_sample_final.RData"))
+load(here("data", "tidy", "voter_history_wide_sample_final.RData"))
 
-# Set up data for plot ----------------------------------------------------
-# Start with finding and extracting relevant columns: 
-df %>% 
-  select(vote_method, contains("date")) %>% 
-  names()
+vote_wide <- wide_profile_temp
+# Set up for plot ---------------------------------------------------------
 
-# Create a column for election-day voting since we don't have that column in
-# the cleaned up data: 
-plot_data <- df %>%
-  mutate(in_person_date = mdy(coalesce(in_person_vote_date_rtn, in_person_vote_date)),
-         mail_in_date = mdy(coalesce(mail_ballot_sent_date_rtn, mail_ballot_sent_date))) %>%
-  mutate(election_day_vote = case_when(
-    in_person_date == mdy("11/03/2020") |
-      mail_in_date == mdy("11/03/2020") ~ "election day",
-    TRUE ~ "other"
-  )) %>%
-  select(county, election_day_vote) %>%
-  group_by(county) %>%
-  table() %>%
-  as.tibble() %>%
-  filter(election_day_vote %in% "election day") %>%
-  rename(election_day = n) %>%
-  select(-election_day_vote)
+# Create data with columns for: 1) Gen 2020 voters, 2) Gen 2016 voters, 
+# and 3) Gen 2020 Results. All grouped by county. 
 
-# Create set of values for in-person, and mail-in votes:
-plot_data_2 <- vote_wide %>%
-  select(county, gen2020) %>%
-  group_by(county) %>%
-  table() %>%
-  as.data.frame() %>%
-  pivot_wider(values_from = Freq, names_from = gen2020) %>%
-  rename("mail_in" = `0`,
-         "in_person" = `1`)
+# Start by recoding--for visualization purposes: 
+vote_wide <- vote_wide %>%
+  mutate(gen2020 = case_when(
+    is.na(gen2020) ~ "no_turnout",
+    gen2020 == 1 ~ "mail_in",
+    gen2020 == 0 ~ "in_person"
+  ),
+  gen2016 = case_when(
+    is.na(gen2016) ~ "no_turnout",
+    gen2016 == 1 ~ "mail_in",
+    gen2016 == 0 ~ "in_person"
+  ))
 
-# County wins:
+# Collapse by grouping by county, with counts recorded:
+vote_wide_plot <- vote_wide %>%
+  group_by(county) %>% 
+  count(gen2016) %>%
+  pivot_wider(names_from = gen2016, values_from = n) %>%
+  mutate(in_person = replace_na(in_person, 0),
+         mail_in = replace_na(mail_in, 0),
+         no_turnout = replace_na(no_turnout, 0)) %>% 
+  mutate(year = 2016)
+
+vote_temp <- vote_wide %>%
+  group_by(county) %>% 
+  count(gen2020) %>%
+  pivot_wider(names_from = gen2020, values_from = n) %>%
+  mutate(in_person = replace_na(in_person, 0),
+         mail_in = replace_na(mail_in, 0),
+         no_turnout = replace_na(no_turnout, 0)) %>% 
+  mutate(year = 2020)
+
+vote_wide_plot <- rbind(vote_wide_plot, vote_temp)
+rm(vote_temp)
+
+# Now pull gen2020 election results: 
 co_pres <- read_csv(paste0("https://raw.githubusercontent.com/",
                            "tonmcg/US_County_Level_Election_Results_08-20/",
                            "master/2020_US_County_Level_Presidential_Results.csv"))
@@ -57,33 +60,20 @@ co_pres_1 <- co_pres %>%
   mutate(county = gsub("\\s*\\w*$", "", county)) %>%
   select(county, result)
 
-  
-# Join all three: 
-plot_data_full <- inner_join(plot_data, plot_data_2, by = "county") %>%
-  inner_join(., co_pres_1, by = "county")
+# Join to create a full tibble: 
+t_plot <- inner_join(co_pres_1, vote_wide_plot, by = "county")
 
-# Plot the data: 
-# Middle segment code from http://www.ggtern.com/2014/01/09/segments-user-request/: 
-lines <- data.frame(x = c(0.5, 0, 0.5), 
-                    y = c(0.5, 0.5, 0), 
-                    z = c(0, 0.5, 0.5), 
-                    xend = c(1, 1, 1)/3, 
-                    yend = c(1, 1, 1)/3, 
-                    zend = c(1, 1, 1)/3)
 
-plot <- ggtern(plot_data_full, aes(election_day, mail_in, in_person)) +
+# Making the plot! 
+ggtern(t_plot, aes(no_turnout, mail_in, in_person)) +
   geom_point(aes(color = result)) +
-  geom_segment(data = lines, 
-               aes(x, y, z, 
-                   xend = xend, yend = yend, zend = zend), 
-               color = 'black', size = 0.5) +
   labs(color = "Outcome") +
-  xlab("Election Day") +
+  xlab("No Turnout") +
   ylab("Mail-In") +
   zlab("In-Person") +
   theme_bw() +
   theme_arrownormal() +
-  scale_color_manual(values = c("Biden" = "blue", "Trump" = "red"))
+  scale_color_manual(values = c("Biden" = "blue", "Trump" = "red")) +
+  geom_line(aes(group = county), size = 0.2)
 
-plot
 
