e55d95b0f483c854e1da2fff2a93f813c1f8a550
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-07-05 18:24:28 -0400

    use `matches` argument to select columns

--- R/07_merged_wide.R
+++ R/07_merged_wide.R
@@ -5,69 +5,9 @@ if (nrows == 100) {
   load(here("data/tidy/voter_history_long_full.RData"))
 }
 
-voter_history_long <-
-  read_fst("data/tidy/sample/voter_history_long_full_hpc.fst")
-
-# Recoding party/gender ========================================================
-voter_history_long %>%
-  select(gender, county_name, election_type, party, voting_method) %>%
-  map(., unique)
-
-voter_history_long <- voter_history_long %>%
-  mutate(
-    party = na_if(party, "no data"),
-    party = na_if(party, " "),
-    party = recode(
-      party,
-      democratic = "dem",
-      republican = "rep",
-      unaffiliated = "uaf",
-      dem = "dem",
-      rep = "rep",
-      uaf = "uaf",
-      .default = "third party"
-    ),
-    gender = recode(
-      gender,
-      male = "male",
-      female = "female",
-      .default = "unknown"
-    ),
-    election_type = recode(
-      election_type,
-      general = "gen",
-      primary = "pri",
-      .default = "oth"
-    )
-  ) %>%
-  filter(!is.na(election_type) & election_type != "oth")
-
-# Recoding voting methods ======================================================
-prop(voter_history_long, "voting_method", sort = TRUE)
-
-## Direct Recording Electronic (DRE)
-mail <- c("mail ballot", "absentee mail", "mail ballot - dre", "absentee carry")
-in_person <- c(
-  "polling place", "in person", "early voting - dre", "early voting",
-  "in person - dre", "vote center", "vote center - dre"
-)
-
-voter_history_long <- voter_history_long %>%
-  mutate(
-    vote_method = case_when(
-      voting_method %in% mail ~ 0,
-      voting_method %in% in_person ~ 1,
-      TRUE ~ NA_real_
-    )
-  )
-
-# Column to aggregate election by year =========================================
-# (removing county-level dependency for the wide pivot)
-voter_history_long <- voter_history_long %>%
-  mutate(election = str_c(election_type, year(election_date)))
-
 # Select variables and pivot to wide =========================================== 
 wide_temp <- voter_history_long %>%
+  filter(election_type != "oth") %>%
   select(voter_id, election, vote_method) %>%
   dedup() %>%
   group_by(voter_id, election) %>%
@@ -77,8 +17,11 @@ wide_temp <- voter_history_long %>%
   pivot_wider(names_from = election, values_from = vote_method) %>%
   select(
     voter_id, 
-    cross2(c("gen", "pri"), seq(2020, 2000, by = -2)) %>%
-      map_chr(~ paste0(.x, collapse = ""))
+    matches(
+      cross2(c("gen", "pri"), seq(2020, 2014, by = -2)) %>%
+        map_chr(~ paste0(.x, collapse = "")) %>%
+        paste(collapse = "|")
+    )
   )
 
 assert_that(!any(duplicated(wide_temp$voter_id)))
