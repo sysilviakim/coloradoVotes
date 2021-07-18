2d840e4cef39130ebb0e183c4d2a870ee09cfa86
Seo-young Silvia Kim <sskim.research@gmail.com>
2021-07-18 13:28:12 -0400

    Use MIT lab's data

---
+++ R/pres_counties.R
@@ -0,0 +1,30 @@
+# County-level presidential results ============================================
+## https://doi.org/10.7910/DVN/VOQCHQ
+
+pres <- read_delim(
+  here("data", "raw", "countypres_2000-2020.tab"), delim = "\t"
+) %>%
+  filter(year >= 2016 & state_po == "CO") %>%
+  mutate(
+    party = case_when(
+      party == "DEMOCRAT" ~ "dem",
+      party == "REPUBLICAN" ~ "rep",
+      TRUE ~ "oth"
+    )
+  ) %>%
+  rename(county = county_name) %>%
+  group_by(year, county, party) %>%
+  summarise(vote = sum(candidatevotes))
+
+pres_wide <- pres %>%
+  pivot_wider(names_from = "party", values_from = "vote") %>%
+  mutate(
+    demp = dem / (dem + oth + rep) * 100,
+    repp = rep / (dem + oth + rep) * 100,
+    othp = 100 - demp - repp
+  ) %>%
+  select(-dem, -oth, -rep) %>%
+  rename(dem = demp, rep = repp, oth = othp) %>%
+  mutate(county = tolower(county))
+
+save(pres_wide, file = here("data", "tidy", "co_county_pres_wide.Rda"))
